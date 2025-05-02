ba4b5f17d5e6e810f4cb0de17d268d429870fa00
RahulGangwar9027 <rahulkrumi97194@gmail.com>
2025-05-02 16:30:05 +0530

    only for testing

--- SchoolERPSYSTEM.Server/Controllers/WeatherForecastController.cs
+++ SchoolERPSYSTEM.Server/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace SchoolERPSYSTEM.Server.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
